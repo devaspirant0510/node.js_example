abbb702a5bf54d7fd2254d68b4a29577aa8be814
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-03-22 14:38:54 +0900

    express 미들웨이

--- node-express/app.js
+++ node-express/app.js
@@ -6,13 +6,40 @@ const app = express();
 const port = 3000;
 
 app.set("port",port);
-app.get('/',(req, res) => {
-    res.sendFile(path.join(__dirname,'index.html'));
 
-})
+
+
+app.use((req, res, next) => {
+    console.log("미들웨어");
+    next();
+
+}/*, (req,res,next)=>{
+  throw new Error("에러났다.");
+}*/);
+
+app.get('/',(req, res) => {
+    //res.sendFile(path.join(__dirname,'index.html'));
+    res.json({hello:"seungho"});
+
+});
+
 app.get("/about",(req, res) => {
     res.send("about");
-})
+});
+
+// 위 사항이 포함되지 않는 모든경우에 쓰임
+// app.use('*',(req, res) => {
+//     res.send("전부다 실행");
+// });
+
+// 에러처리
+// 사용자에게 에러코드를 보여주지 않고 콘솔에만 보여줌
+app.use((err,req, res, next) => {
+    console.error(err);
+    res.status(200).send("에러입니다");
+});
+
+
 app.listen(app.get("port"),()=>{
     console.log(`127.0.0.1:${app.get("port")}`);
 });
